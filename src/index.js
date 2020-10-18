2ac3d09ae3b7c177ec79a998725312bc71ebeed4
kelly_maass <kmaass77627@gmail.com>
2020-10-17 23:56:43 -0500

    neww

---
+++ src/index.js
@@ -0,0 +1,23 @@
+console.log("testing...")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const BACKEND_URL = 'https://localhost:3001';
+// fetch(`${BACKEND_URL}/test`)
+//   .then(response => response.json())
+//   .then(parsedResponse => console.log(parsedResponse));
